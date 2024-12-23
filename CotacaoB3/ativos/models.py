9587f981fd8ef19dbb81efe0bc4e6eabbdac680b
LukasFalk25 <134344586+LukasFalk25@users.noreply.github.com>
2024-12-23 18:10:20 -0300

    feat: implementei os modelos Ativo e Cotação no app 'ativos'

--- CotacaoB3/ativos/models.py
+++ CotacaoB3/ativos/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Ativo(models.Model):
+    nome = models.CharField(max_length=20)
+    codigo = models.CharField(max_length=10, unique=True)
+    limite_inferior = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    limite_superior = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    periodicidade = models.PositiveIntegerField(help_text="Periodicidade em minutos")
+    
+    def __str__(self):
+        return f"{self.nome} ({self.codigo})"
+    
+class Cotacao(models.Model):
+    ativo = models.ForeignKey(Ativo, on_delete=models.CASCADE, related_name="cotacoes")
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    data_hora = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.ativo.codigo} - {self.preco} ({self.data_hora})"
